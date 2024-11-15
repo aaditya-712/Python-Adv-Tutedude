4e88e0fbf352d4a9cdc78f3ae0d617e01a62550d
Aaditya  Deshmukh <aadityagdeshmukh9975@gmail.com>
2024-11-15 13:06:59 +0530

    table created

--- Visual_Studio/demo.py
+++ Visual_Studio/demo.py
@@ -1,1 +1,10 @@
-print("Hello Python")
+import psycopg2
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="1234", host="localhost", port="5433")
+
+cursor = conn.cursor()
+cursor.execute('''create table employees(Name Text, ID int, Age int);''')
+print("Table Created Successfully.")
+
+conn.commit()
+conn.close()
+
