a8247d5600429bbdee3e64ec0d69023f124c225e
Aaditya  Deshmukh <aadityagdeshmukh9975@gmail.com>
2024-11-15 13:13:17 +0530

    data addded

--- Visual_Studio/demo.py
+++ Visual_Studio/demo.py
@@ -1,10 +1,22 @@
 import psycopg2
-conn = psycopg2.connect(dbname="postgres", user="postgres", password="1234", host="localhost", port="5433")
+def table():
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="1234", host="localhost", port="5433")
 
-cursor = conn.cursor()
-cursor.execute('''create table employees(Name Text, ID int, Age int);''')
-print("Table Created Successfully.")
+    cursor = conn.cursor()
+    cursor.execute('''create table employees(Name Text, ID int, Age int);''')
+    print("Table Created Successfully.")
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
 
+def data():
+    conn = psycopg2.connect(dbname="postgres", user="postgres", password="1234", host="localhost", port="5433")
+
+    cursor = conn.cursor()
+    cursor.execute('''insert into employees(Name, ID, Age) values('Aaditya', 01, 20);''')
+    print("Data Added Successfully.")
+
+    conn.commit()
+    conn.close()
+
+data()
