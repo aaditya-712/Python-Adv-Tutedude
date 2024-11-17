5ac5733b68729fbc30334dff92a86fa8c9b030f5
Aaditya  Deshmukh <aadityagdeshmukh9975@gmail.com>
2024-11-17 15:58:39 +0530

    added new

---
+++ 11_Django/Adding_Numbers/Add_num/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hello(request):
+    return render(request, 'home.html', {'name':'Tom'})
+
+def add(request):
+    value_1 = int(request.GET['number 1'])
+    value_2 = int(request.GET['number 2'])
+    res = value_1 + value_2
+    return render(request, 'result.html', {'result':res})
