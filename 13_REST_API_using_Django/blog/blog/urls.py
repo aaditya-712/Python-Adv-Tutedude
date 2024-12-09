0342524d8e13f25bf387679673baabd559c6cd10
Aaditya  Deshmukh <aadityagdeshmukh9975@gmail.com>
2024-12-09 21:56:13 +0530

    REST api

--- 13_REST_API_using_Django/blog/blog/urls.py
+++ 13_REST_API_using_Django/blog/blog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from helloworld.views import HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello', HelloWorldView.as_view()),
 ]
